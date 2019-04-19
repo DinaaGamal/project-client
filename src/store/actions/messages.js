bc74d4f539dc99c3a27c4d6894a6e484a182b901
dina <dina@gmail.com>
2019-04-20 01:51:24 +0200

    fixx

--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -1,37 +1,40 @@
-import { apiCall } from "../../services/api";
-import { REMOVE_MESSAGE, ADD_MESSAGE, LOAD_MESSAGES } from "../actionsTypes";
-import { addError } from "./errors";
-import history from "../../history";
+import { apiCall } from '../../services/api';
+import { REMOVE_MESSAGE, ADD_MESSAGE, LOAD_MESSAGES } from '../actionsTypes';
+import { addError, removeError } from './errors';
+import history from '../../history';
 
-export const loadMessages = messages => ({
-  type: LOAD_MESSAGES,
-  messages
+export const loadMessages = (messages) => ({
+	type: LOAD_MESSAGES,
+	messages
 });
 
-export const remove = id => ({
-  type: REMOVE_MESSAGE,
-  id
+export const remove = (id) => ({
+	type: REMOVE_MESSAGE,
+	id
 });
 
-export const removeMessage = (userId, messageId) => dispatch => {
-  return apiCall("delete", `/api/users/${userId}/messages/${messageId}`)
-    .then(() => dispatch(remove(messageId)))
-    .catch(err => dispatch(addError(err.message)));
+export const removeMessage = (userId, messageId) => (dispatch) => {
+	return apiCall('delete', `/api/users/${userId}/messages/${messageId}`)
+		.then(() => dispatch(remove(messageId)))
+		.catch((err) => dispatch(addError(err.message)));
 };
 
-export const fetchMessages = () => dispatch => {
-  return apiCall("get", "/api/messages")
-    .then(res => dispatch(loadMessages(res)))
-    .catch(err => dispatch(addError(err.message)));
+export const fetchMessages = () => (dispatch) => {
+	return apiCall('get', '/api/messages')
+		.then((res) => dispatch(loadMessages(res)))
+		.catch((err) => dispatch(addError(err.message)));
 };
 
-export const postNewMessage = text => (dispatch, getState) => {
-  let { currentUser } = getState();
-  let id = currentUser.user.id;
-  return apiCall("post", `/api/users/${id}/messages`, { text })
-    .then(res => {
-      dispatch(loadMessages(res));
-      history.push("/");
-    })
-    .catch(err => dispatch(addError(err.message)));
+export const postNewMessage = (text) => async (dispatch, getState) => {
+	try {
+		let { currentUser } = getState();
+		let id = currentUser.user.id;
+		const res = await apiCall('post', `/api/users/${id}/messages`, { text });
+		if (res) {
+			// await dispatch(loadMessages(res));
+			history.push('/');
+		}
+	} catch (error) {
+		dispatch(addError(error.message));
+	}
 };
